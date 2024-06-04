b621b395b17cfc5ec9f3f77bdb0226b29a3d9105
Gunzerix <arsanautikagavian@outlook.co.id>
2024-06-04 16:44:26 +0700

    kerjaan theo (6/4/2024)

--- src/Assets/AmsterdamCityBreaksItems.js
+++ src/Assets/AmsterdamCityBreaksItems.js
@@ -1,59 +1,59 @@
-import AmsterdamHotel1 from '../Assets/Images/carieer.jpeg';
-import AmsterdamHotel2 from '../Assets/Images/sepatu 1.jpeg';
-import AmsterdamHotel3 from '../Assets/Images/sepatu 2.jpeg';
-import AmsterdamHotel4 from '../Assets/Images/tenda.jpeg';
-import AmsterdamHotel5 from '../Assets/Images/sleepingbag.jpeg';
-import AmsterdamHotel6 from '../Assets/Images/headlamp.jpeg';
+import AmsterdamHotel1 from '../Assets/Images/crier.jpg';
+import AmsterdamHotel2 from '../Assets/Images/spatu.jpg';
+import AmsterdamHotel3 from '../Assets/Images/spatutrail.jpg';
+import AmsterdamHotel4 from '../Assets/Images/tent.jpg';
+import AmsterdamHotel5 from '../Assets/Images/sb.jpg';
+import AmsterdamHotel6 from '../Assets/Images/headlmp.jpg';
 import { CardImg } from 'react-bootstrap';
 
 let AmsterdamCityBreaksItems = [
     {
         id: 1,
         itemImage: AmsterdamHotel1,
-        itemTitle: 'Tas Carieer ',
-        itemSubTitle: 'Tas Carier dengan ukuran 60 liter',
-        itemNights: '3 hari',
-        itemPrice: 'Rp 150.000'
+        itemTitle: 'Carrier 45L ',
+        itemSubTitle: 'Carrier yang dirancang untuk kegiatan hiking selama 1-3 hari. Ransel berkapasitas 45 Liter ini dilengkapi dengan fitur backsystem Fit Light yang dapat memberikan kenyamanan dan meminimalisir panas pada punggung Anda.',
+        itemNights: '1 hari',
+        itemPrice: 'Rp. 35.000'
     },
     {
         id: 2,
         itemImage: AmsterdamHotel2,
         itemTitle: 'Sepatu Gunung',
-        itemSubTitle: 'Sepatu gunung berkualitas',
-        itemNights: '3 hari',
-        itemPrice: 'Rp 100.000'
+        itemSubTitle: 'Sepatu Gunung yang dirancang untuk kegiatan hiking dan treking dengan potongan mid cut yang memberikan proteksi lebih pada kaki saat beraktifitas. ',
+        itemNights: '1 hari',
+        itemPrice: 'Rp. 25.000'
     },
     {
         id: 3,
         itemImage: AmsterdamHotel3,
-        itemTitle: 'Sepatu Gunung Salomon',
-        itemSubTitle: 'Sepatu gunung berkualitas',
-        itemNights: '3 hari',
-        itemPrice: 'Rp 200.000'
+        itemTitle: 'Sepatu Trail Run',
+        itemSubTitle: 'Sepatu yang memiliki desain khusus, seperti sol tebal dan juga outsole bergerigi. Spesifikasi sepatu trail running tidak sama dengan sepatu lari pada umumnya.',
+        itemNights: '1 hari',
+        itemPrice: 'Rp 30.000'
     },
     {
         id: 4,
         itemImage: AmsterdamHotel4,
-        itemTitle: 'Tenda',
-        itemSubTitle: 'Tenda berkapsitas 6 orang',
-        itemNights: '3 hari',
-        itemPrice: 'Rp 300.000'
+        itemTitle: 'Tenda 4P',
+        itemSubTitle: 'Tenda Kapasitas 4 Orang cocok digunakan untuk camping maupun aktifitas outdoor lainnya, dilengkapi dengan fitur double layer dan bahan waterproof yang membuat tidak perlu khawatir menghadapi cuaca hujan.',
+        itemNights: '1 hari',
+        itemPrice: 'Rp. 40.000'
     },
     {
         id: 5,
         itemImage: AmsterdamHotel5,
         itemTitle: 'Sleeping bag',
-        itemSubTitle: 'Sleeping bag berbahan bulu angsa',
-        itemNights: '2 hari',
-        itemPrice: 'Rp 150.000'
+        itemSubTitle: 'Sleeping Bag Outdoor Hiking Hangat Tebal Kualitas Terbaik, Mampu Menahan Suhu dingin Hingga 10-15 derajat dan Ringan, mudah di bawa kemanapun.',
+        itemNights: '1 hari',
+        itemPrice: 'Rp. 25.000'
     },
     {
         id: 6,
         itemImage: AmsterdamHotel6,
         itemTitle: 'Head lamp',
-        itemSubTitle: 'Senter kepala',
-        itemNights: '3 hari',
-        itemPrice: 'Rp 50.000'
+        itemSubTitle: 'Headlamp yang sangat cocok digunakan untuk kegiatan outdoor seperti kemping, hiking, trekking, dan susur gua. Dapat menyala hingga 48 jam, tahan air dengan level IPX6. ',
+        itemNights: '1 hari',
+        itemPrice: 'Rp. 15.000'
     }
 ];
 
--- src/Assets/ParisCityBreaksItems.js
+++ src/Assets/ParisCityBreaksItems.js
@@ -1,58 +1,58 @@
-import FranceHotel1 from '../Assets/Images/france-hotel1.jpg';
-import FranceHotel2 from '../Assets/Images/france-hotel2.jpg';
-import FranceHotel3 from '../Assets/Images/france-hotel3.jpg';
-import FranceHotel4 from '../Assets/Images/france-hotel4.jpg';
-import FranceHotel5 from '../Assets/Images/france-hotel5.jpg';
-import FranceHotel6 from '../Assets/Images/france-hotel6.jpg';
+import FranceHotel1 from '../Assets/Images/tent1p.jpg';
+import FranceHotel2 from '../Assets/Images/nesting.jpg';
+import FranceHotel3 from '../Assets/Images/boots.jpg';
+import FranceHotel4 from '../Assets/Images/waterbladder.jpg';
+import FranceHotel5 from '../Assets/Images/sbmummy.jpg';
+import FranceHotel6 from '../Assets/Images/carrier75.jpg';
 
 let ParisCityBreaksItems = [
     {
         id: 1,
         itemImage: FranceHotel1,
-        itemTitle: 'Paris hotel',
-        itemSubTitle: 'France, Paris',
-        itemNights: '3 nights',
-        itemPrice: '£599'
+        itemTitle: 'TENDA 1P',
+        itemSubTitle: 'Adalah tenda untuk kapasitas 1 orang yang cocok digunakan untuk kegiatan berkemah. Tenda ini memiliki tiang rangka berbahan aluminium yang mudah dipasangkan, satu akses pintu masuk dan mesh sebagai ventilasi.',
+        itemNights: '1 hari',
+        itemPrice: 'Rp. 25.000'
     },
     {
         id: 2,
         itemImage: FranceHotel2,
-        itemTitle: 'Castle hotel',
-        itemSubTitle: 'France, Paris',
-        itemNights: '3 nights',
-        itemPrice: '£599'
+        itemTitle: 'Alat masak 3 in 1',
+        itemSubTitle: 'Terbuat dari bahan alumunium dengan senyawa anodized dan stainless steel pada bagian gagangnya membuat panci ini menjadi lebih kokoh dan mudah dicuci dibandingkan dengan bahan alumunium pada umumnya. ',
+        itemNights: '1 hari',
+        itemPrice: 'Rp. 10.000'
     },
     {
         id: 3,
         itemImage: FranceHotel3,
-        itemTitle: 'Old hotel',
-        itemSubTitle: 'France, Paris',
-        itemNights: '2 nights',
-        itemPrice: '£799'
+        itemTitle: 'Hiking Boots',
+        itemSubTitle: 'Dirancang untuk mendukung aktivitas luar ruang Anda di area hutan hujan. Material kokoh dilengkapi insole Ortholite yang breathable (memiliki daya evaporasi tinggi yang mampu menguapkan kelembapan agar cepat kering).',
+        itemNights: '1 hari',
+        itemPrice: 'Rp. 20.000'
     },
     {
         id: 4,
         itemImage: FranceHotel4,
-        itemTitle: 'France hotel',
-        itemSubTitle: 'France, Paris',
-        itemNights: '3 nights',
-        itemPrice: '£399'
+        itemTitle: 'Water Bladder',
+        itemSubTitle: 'Dirancang untuk menjaga Anda tetap terhidrasi, water bladder dengan kapasitas yang mampu menampung air sebanyak 2 liter ini merupakan perlengkapan yang tepat untuk aktivitas outdoor Anda. ',
+        itemNights: '1 hari',
+        itemPrice: 'Rp. 15.000'
     },
     {
         id: 5,
         itemImage: FranceHotel5,
-        itemTitle: 'Paris hotel',
-        itemSubTitle: 'France, Paris',
-        itemNights: '2 nights',
-        itemPrice: '£499'
+        itemTitle: 'Sleeping Bag',
+        itemSubTitle: 'Dilengkapi dengan teknologi Tropic Insulation, produk ini dapat menjaga tubuh tetap hangat ketika suhu menjadi dingin, khususnya di daerah dengan suhu 5 sehingga Anda bisa tetap merasa aman dan nyaman pada saat tidur. ',
+        itemNights: '1 hari',
+        itemPrice: 'Rp. 30.000'
     },
     {
         id: 6,
         itemImage: FranceHotel6,
-        itemTitle: 'Grand hotel',
-        itemSubTitle: 'France, Paris',
-        itemNights: '3 nights',
-        itemPrice: '£299'
+        itemTitle: 'Carrier 75+15L',
+        itemSubTitle: 'Adalah carrier yang dirancang untuk kegiatan ekspedisi dan trekking berdurasi panjang (lebih dari 5 hari), dilengkapi dengan teknologi backsystem Ergo Spino Dual Frame yang dapat membagi beban secara merata yang dapat menjaga tulang belakang dari resiko cedera.',
+        itemNights: '1 hari',
+        itemPrice: 'Rp. 75.000'
     }
 ];
 
